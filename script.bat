0d7597073789d29fa828e8fbbb79fbbe900fc246
mnDevs <43381188+mn-Devs@users.noreply.github.com>
2020-06-04 13:27:14 +0200

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCuDJGyX8VAjFD9bWwqOM2q+FYku4evw0+2Vo0sUV/EDcYzU1PqHI+9z
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSTk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpSI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZks0
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
-::ZQ05rAF9IAHYFVzEqQICIRhfLA==
-::eg0/rx1wNQPfEVWB+kM9LVsJDDaLP2D6NLoQ5Ig=
-::fBEirQZwNQPfEVWB+kM9LVsJDDSQM2G/BaFc2uH45Io=
-::cRolqwZ3JBvQF1fEqQIEIBJHDC2RfFmzBb58
-::dhA7uBVwLU+EWFSK0EcmO3s=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATEphJieFtZQiCHKnja
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCuDJGyX8VAjFD9bWwqOM2q+FYku4evw0/mEsEQNQN4pKbPJ07eFbuUL7yU=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 
 
